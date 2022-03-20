520183486c054eb6ee90c0697231b598767b2a0a
christounetcom <nick.christou@protonmail.com>
2022-03-20 16:56:45 +1100

    uploading code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let returnStr = "";
+    if (num >= 0) {
+    for (i = 0; i < num; i++) {
+        returnStr += str;        }
+    } else {
+        return "ERROR";
+    }
+    
+    return returnStr;
 };
-
+//repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
