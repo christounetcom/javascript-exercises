a715787bd016827a941d40a56d1d3493c14231f4
christounetcom <nick.christou@protonmail.com>
2022-03-20 17:23:36 +1100

    uploading exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversedString ='';
 
+    for (let i = str.length; i >= 0; i--) {
+        reversedString += str[i];
+    };
+    return reversedString;
 };
 
+reverseString('hello there');
 // Do not edit below this line
 module.exports = reverseString;
